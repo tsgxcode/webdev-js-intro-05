e1b9ffe5ee56e847a3db7d6bd5bdc1cc9dbbece8
Dan Collins <dan.collins.dev@gmail.com>
2024-12-23 09:54:32 -0500

    initial commit

---
+++ js/assignment.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const todos = [
+    "Wash the dishes",
+    "Clean the gutters",
+    "Mow the yard",
+    "Do some coding",
+    "Rake the leaves",
+];
+
+// Declare your two immutable variables
+
+
+// Declaring constant variables to store references to the elements
+// that will be updated by your code.
+const answerOneEl = document.getElementById("answer-one");
+const answerTwoEl = document.getElementById("answer-two");
+const answerThreeEl = document.getElementById("answer-three");
+const answerFourEl = document.getElementById("answer-four");
+const answerFiveEl = document.getElementById("answer-five");
+const answerSixEl = document.getElementById("answer-six");
+const submissionBtn = document.getElementById("submission-btn");
+
+function updateAnswerOne() {
+    
+}
+
+function updateAnswerTwo() {
+
+}
+
+function removeLastValue() {
+
+}
+
+function removeFirstValue() {
+
+}
+
+function addShiftAndPopValues() {
+
+}
+
+function updateAnswerFour() {
+
+}
+
+function reverseTodoList() {
+
+}
+
+function updateAnswerFive() {
+
+}
+
+function updateAnswerSix() {
+
+}
+
+function render() {
+
+}
+
+submissionBtn.addEventListener("click", function () {
+    // Update this function
+    render();
+});
