440fe83a7ffddfa9b3575b4354622a0c7a7ec761
Scott <tsgcats@gmail.com>
2025-01-20 00:05:44 -0500

    Part one complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+//MyVar = "Exper": // Attempting to a Var that is not declared
+
+//let x = 9; //Var is declared
+//console.log(x); 
